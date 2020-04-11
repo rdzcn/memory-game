3bdda6149262b353984821f3104e0f1e8d6462c5
Arda Özcan <arda.oezcan@aroundhome.de>
2020-04-11 20:19:41 +0200

    moved /styles and added node-sass

--- memory-ui/src/index.js
+++ memory-ui/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import "../styles/index.css";
+import "./styles/index.scss";
 import App from "./App";
 
 ReactDOM.render(
