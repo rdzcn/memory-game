0e3d15492dcbbd94948edd7cf87275a6acfdca69
Arda Özcan <arda.oezcan@aroundhome.de>
2020-04-11 19:26:59 +0200

    context and pageWrapper

--- memory-ui/src/pages/Landing/Landing.jsx
+++ memory-ui/src/pages/Landing/Landing.jsx
@@ -1,20 +1,9 @@
 import React, { useEffect } from "react";
-import io from "socket.io-client";
 import { Row, Column } from "../../components/grid";
 import { Header } from "./Landing.styles";
 
 const Landing = () => {
-  const URL = window.location.href;
-  const socket = io(URL);
-
-  useEffect(() => {
-    if (socket) {
-      socket.on("connect",() => {
-        console.log("SOCKET-ID-LANDING", socket.id);
-      });
-    }
-  }, [socket]);
-
+ 
   return (
     <Row>
       <Column span={{ mobile: 4, tablet: 3, desktop: 3 }}>
