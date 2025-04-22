3395ed0fe011cefb4f9c93473c8bea76f09da54e
Rdzcn <arda.oezcan@procilon.de>
2025-04-23 01:02:43 +0200

    add packages/common

---
+++ packages/common/app/types/game-types.ts
@@ -0,0 +1,45 @@
+export type GameStatus = "waiting" | "playing" | "finished";
+
+export interface Player {
+	id: string;
+	name: string;
+	score: number;
+}
+
+export interface GameState {
+	title: string;
+	players: Player[];
+	id: string;
+	currentTurn: string;
+	status: GameStatus;
+	flippedCards: Card[];
+	lastFlippedCard: Card | undefined;
+	winner: Player | undefined;
+	cards: Card[];
+	cardCount: number;
+	createdAt: number;
+	startedAt?: number;
+	finishedAt?: number;
+	gameScore: number;
+	totalMoves: number;
+}
+
+export interface Card {
+	id: number;
+	value: string;
+	isFlipped: boolean;
+	isMatched: boolean;
+}
+
+export interface CardData {
+	id: number;
+	value: string;
+}
+
+export interface PlayerConnection {
+	playerId: string;
+	gameId: string;
+	lastHeartbeat: number;
+	missedBeats: number;
+	socketId?: string;
+}
